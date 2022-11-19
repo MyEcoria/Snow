d7e31d2c9354dd9f911b7a780807752fbe3d47b3
MyEcoria <103332030+MyEcoria@users.noreply.github.com>
2022-11-19 13:06:55 +0100

    Update flyby_listings.js

--- js/flyby_listings.js
+++ js/flyby_listings.js
@@ -1,25 +1,7 @@
-/*
-    To have your site advertised on SomeNano Snow, visit https://somenano.com/advertise
-
-    NanoCenter projects will receive free advertisement unless enough paid advertisements are listed
-*/
 var advert_listing = [
     {
-        'text': 'Advertise here for 5 Nano per month',
-        'url': 'https://somenano.com/advertise?ref=snow'
-    },
-    {
-        'text': 'NanoCenter Nano Green Initiative',
-        'url': 'https://nanocenter.org/projects/nanogreenintiative',
-    },
-    {
-        'text': 'NanoCenter #NanoTrashTag Bounty',
-        'url': 'https://nanocenter.org/projects/TrashTag',
-    },
-    {
-        'text': 'Buy plushies with Nano',
-        'url': 'https://loveplushy.com/blogs/guides/buy-plushies-with-crypto',
-        'expires': 'Wed Mar 30 2022 09:56:03 GMT-0400 (Eastern Daylight Time)'
+        'text': 'Advertise here for 1000 DogeNano per month (Discord: MyEcoria#2654)',
+        'url': 'mailto:contact@myecoria.com'
     }
 ];
 
